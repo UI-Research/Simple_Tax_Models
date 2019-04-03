670c2e958f8963952937d5aa9d15d241e316d32c
Jessica <uijkelly@users.noreply.github.com>
2019-04-03 11:11:56 -0400

    Update run_fortran.sh
    removing run of summary

--- run_fortran.sh
+++ run_fortran.sh
@@ -2,9 +2,9 @@
 echo "running fortran"
 ./fortmodel
 echo "fortran complete"
-echo "running summarize"
-python ../summarize/create_summary_tables.py output.csv
-echo "summarize complete"
+#echo "running summarize"
+#python ../summarize/create_summary_tables.py output.csv
+#echo "summarize complete"
 #note that gets written to the directory we are in now
 #will need to get copied somewhere to save
-cat summary_output.csv
+#cat summary_output.csv
