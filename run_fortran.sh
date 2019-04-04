f0710a737557ddf085b90ea44df4ec479196fe69
Jessica <jkelly@urban.org>
2019-04-04 13:53:43 -0400

    added fortran run parameter path adjustment

--- run_fortran.sh
+++ run_fortran.sh
@@ -2,13 +2,16 @@
 # expects two arguments, first for parameter file, second for output file
 # copy the parameter file
 KEY="s3://mic.urban.org/tpc/simple-tax-model/""$1"
-echo "Attempting download : $KEY as $1"
-
+echo "Attempting download : $KEY"
+# copy parameter file
 for KEY
 do
   aws s3 cp "$KEY" /param
 done
+PARAMETER_PATH="/param/$1"
 
 echo "running fortran"
-./fortmodel "$1" "$2"
+./fortmodel "$PARAMETER_PATH" "$2"
 echo "fortran complete"
+
+# copy results
