ab1c26e255e08cff4aca5ed758e3e5b1887a1ac2
Jessica <jkelly@urban.org>
2019-04-05 09:09:16 -0400

    more s3 cp debugging

--- run_fortran.sh
+++ run_fortran.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # expects two arguments, first for parameter file, second for output file
 # copy the parameter file
-KEY=s3://mic.urban.org/tpc/simple-tax-model/parameterOptions_Standard_1600.csv
-echo "Attempting download : $KEY"
+FILE="s3://mic.urban.org/tpc/simple-tax-model/$1"
+echo "Attempting download : $FILE"
 # copy parameter file
-for KEY
-do
-  aws s3 cp s3://mic.urban.org/tpc/simple-tax-model/parameterOptions_Standard_1600.csv parameterOptions_Standard_1600.csv
-done
+#for FILE
+#do
+#  echo " want --- aws s3 cp s3://mic.urban.org/tpc/simple-tax-model/parameterOptions_Standard_1600.csv parameterOptions_Standard_1600.csv"
+#  echo " is   --- aws s3 cp $FILE $1"
+  #aws s3 cp s3://mic.urban.org/tpc/simple-tax-model/parameterOptions_Standard_1600.csv parameterOptions_Standard_1600.csv
+aws s3 cp $FILE $1
+#done
 
 
 echo "running fortran"
-./fortmodel "$1" "$2"
+./Fortran/fortmodel "$1" "$2"
 echo "fortran complete"
 
 # copy results
