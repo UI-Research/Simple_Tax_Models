ded1a853357014cda8bdbacadeab9589e1ca4d2f
Jessica <uijkelly@users.noreply.github.com>
2019-07-02 14:41:09 -0400

    Update run_fortran.sh

--- run_fortran.sh
+++ run_fortran.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 # expects two arguments, first for parameter file, second for output file
 # copy the parameter file
-FILE="s3://mic.urban.org/tpc/simple-tax-model/$1"
+FILE="s3://your_input_bucket/$1"
 echo "Attempting download : $FILE"
 # copy parameter file
-#for FILE
-#do
-#  echo " want --- aws s3 cp s3://mic.urban.org/tpc/simple-tax-model/parameterOptions_Standard_1600.csv parameterOptions_Standard_1600.csv"
-#  echo " is   --- aws s3 cp $FILE $1"
-  #aws s3 cp s3://mic.urban.org/tpc/simple-tax-model/parameterOptions_Standard_1600.csv parameterOptions_Standard_1600.csv
 aws s3 cp $FILE $1
 #done
 
@@ -19,5 +14,5 @@ echo "running fortran"
 echo "fortran complete"
 
 # copy results
-OUTFILE="s3://mic.urban.org/tpc/simple-tax-model-out/$2.csv"
+OUTFILE="s3://your_output_bucket/$2.csv"
 aws s3 cp $OUTNAME $OUTFILE
